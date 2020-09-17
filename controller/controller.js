4466485de1b5a2ff3df55da38707edd70c50323f
ManoloPin <jpinillaca1@uniminuto.edu.co>
2020-09-16 21:41:24 -0500

    añadi funcion seno

--- controller/controller.js
+++ controller/controller.js
@@ -2,26 +2,35 @@
 var xv = 0;
 var yv = 0;
 var tam = 0;
-function dibujar(tamaño,x, y) {
+var a = 0;
+var f = 0;
+var amplitude = 100;
+var frequency = 50;
+
+function dibujar(tamaño, x, y) {
     document.getElementById("grafico").innerHTML = '<svg width="800" height="600">'
-        // + '<circle cx="' + x + '" cy="' + y + '" r="' + radio + '" stroke="green" stroke-width="4" fill="yellow" />'
-        + '<rect x="' + x + '" y="' + y + '" width="'+tamaño+'" height="'+tamaño+'"" stroke="green" stroke-width="4" fill="yellow" />'
+       
+        + '<rect x="' + x + '" y="' + y + '" width="' + tamaño + '" height="' + tamaño + '"" stroke="green" stroke-width="4" fill="yellow" />'
         + '</svg>';
 }
 
-function anim(tamaño,x, y) {
+function anim(tamaño, x, y,amplitude,frequency) {
     xv = x;
     yv = y;
+    xh = x;
+    yh = y;
     tam = tamaño;
+    a = amplitude;
+    f = frequency;
     myVar = setInterval(show, 60);
 }
 
 function show() {
-    xv++;  // controlan el movimiento 
-    yv++;
+    xv++;
+    yv = ((200/2) + amplitude * Math.cos(yh/frequency));
+    xh++;
+    yh++;
     document.getElementById("grafico").innerHTML = '<svg width="800" height="600">'
-    //  + '<circle cx="' + xv + '" cy="' + yv + '" r="' + rad +'" stroke="green" stroke-width="4" fill="blue" />'
-        + '<rect x="' + xv + '" y="' + yv + '" width="'+tam+'" height="'+tam+'"" stroke="green" stroke-width="4" fill="yellow" />'
+        + '<rect x="' + xv + '" y="' + yv + '" width="' + tam + '" height="' + tam + '"" stroke="green" stroke-width="4" fill="yellow" />'
         + '</svg>';
-    
 }
