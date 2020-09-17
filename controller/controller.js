98461e19eaf4ac8662e99f35e7db23d8982bad89
ManoloPin <jpinillaca1@uniminuto.edu.co>
2020-09-16 23:06:54 -0500

    cambio-3

--- controller/controller.js
+++ controller/controller.js
@@ -6,28 +6,22 @@ var a = 0;
 var f = 0;
 var amplitude = 100;
 var frequency = 50;
+var tim = 0;
 
-function dibujar(tamaño) {
-    document.getElementById("grafico").innerHTML = '<svg width="1800" height="350">'
-
-        + '<rect x="' + 70 + '" y="' + 200 + '" width="' + tamaño + '" height="' + tamaño + '"" stroke="green" stroke-width="4" fill="yellow" />'
-        + '</svg>';
-}
-
-function anim(tamaño, amplitude, frequency) {
+function anim(tamaño, amplitude, frequency,tiempo) {
     xv = 70;
     yv = 200;
     yh = yv;
     tam = tamaño;
     a = amplitude;
     f = frequency;
-    myVar = setInterval(show, 10);
+    tim = tiempo;
+    myVar = setInterval(show, tim);
 }
 
 function show() {
     xv++;
-    yv = ((200 / 2) + amplitude * Math.cos(yh++ / frequency));
-
+    yv = ((300 / 2) + amplitude * Math.cos(yh++ / frequency));
     document.getElementById("grafico").innerHTML = '<svg width="1800" height="350">'
         + '<rect x="' + xv + '" y="' + yv + '" width="' + tam + '" height="' + tam + '"" stroke="green" stroke-width="4" fill="yellow" />'
         + '</svg>';
